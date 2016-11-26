d1cd615c0fca41d04f177c0a34f61e91fe1f14f7
Azize ulak <azizeulak@azizes-air.mynet>
2016-11-26 21:34:33 +0200

    some meaningful message goes here

---
+++ myapp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from myapp.models import (Book, Author)
+
+
+class BookAdmin(admin.ModelAdmin):
+    pass
+    list_display = ('title', 'lc_classification')
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+    list_display = ('name', 'surname', 'date_of_birth')
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+# Register your models here
---
+++ myapp/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+
+    def __str__(self):                 # __unicode__ on Python 2
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ForeignKey(Author, on_delete=models.CASCADE)
+    lc_classification = models.CharField(max_length=100)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return "%s" % self.title
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+
