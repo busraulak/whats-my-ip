341d9ce6363a495c912b47a2c2d678bbc6482df3
busraulak <busra.ulak@metglobal.com>
2016-11-28 16:21:37 +0200

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -9,12 +9,3 @@ def viewArticle(request, articleId):
    text = "Displaying article Number : %s"%articleId
    return HttpResponse(text)
 
-from django.http import HttpResponse
-import datetime
-
-now = datetime.datetime.now().strftime('%H:%M:%S')
-
-def post_list(request):
-    ip = request.META['REMOTE_ADDR']
-    return HttpResponse("Merhaba, {}, saat su an {}".format(ip, now))
-
