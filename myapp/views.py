545d79221bb050e0fc23f1cdc0d786e6f2c21351
busraulak <busra.ulak@metglobal.com>
2016-11-28 16:13:41 +0200

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -8,7 +8,7 @@ def hello(request):
 def viewArticle(request, articleId):
    text = "Displaying article Number : %s"%articleId
    return HttpResponse(text)
-=======
+
 from django.http import HttpResponse
 import datetime
 
@@ -17,4 +17,4 @@ now = datetime.datetime.now().strftime('%H:%M:%S')
 def post_list(request):
     ip = request.META['REMOTE_ADDR']
     return HttpResponse("Merhaba, {}, saat su an {}".format(ip, now))
->>>>>>> 47e9abd8d7de15ddd040ba0b45410a37684185af
+
